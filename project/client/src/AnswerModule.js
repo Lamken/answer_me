e73b0901bc19e19c46b99a67cb728507caf46387
lamken <lamken16033@gmail.com>
2019-01-01 22:13:09 +0800

    update answer module

--- project/client/src/AnswerModule.js
+++ project/client/src/AnswerModule.js
@@ -3,37 +3,29 @@ import React from 'react'
 import {Button,Panel} from "react-bootstrap";
 
 class ChoiceForm extends React.Component {
-//   constructor(props){
-//     super(props);
-//     console.log()
-//   } 
 
   render() {
     return (
-      <Panel bsStyle="primary" id="choicepanel">
-      <Panel.Heading>
-        <Panel.Title componentClass="h3">主菜单</Panel.Title>
-        </Panel.Heading>
-          <Panel.Body>
-          <form>
-          <div id="userchoice">
-          <Button bsStyle="success btn-lg"  name="release" type="submit" onClick={(event) => {
-          this.props.userChoice(event.target.name);
-          }}>发起委托</Button> 
-          </div>
-          <div id="userchoice">
-          <Button bsStyle="success btn-lg"   name="query" type="submit" onClick={(event) => {
-          this.props.userChoice(event.target.name);
-          }}>查看委托</Button>
-          </div>
-          <div id="userchoice">
-          <Button bsStyle="success btn-lg"   name="queryself" type="submit" onClick={(event) => {
-          this.props.userChoice(event.target.name);
-          }}>被接受的委托</Button> 
-          </div>
-        </form>
-      </Panel.Body>
-    </Panel>
+      <div class="Dropzone RichText ztext" style="min-height: 118px;">
+        <div class="DraftEditor-root">
+            <div class="public-DraftEditorPlaceholder-root">
+                <div class="public-DraftEditorPlaceholder-inner" id="placeholder-5mdts" style="white-space: pre-wrap;">写回答...</div>
+            </div>
+            <div class="DraftEditor-editorContainer">
+                <div aria-describedby="placeholder-5mdts" class="notranslate public-DraftEditor-content" contenteditable="true"
+                    role="textbox" spellcheck="true" tabindex="0" style="outline: none; user-select: text; white-space: pre-wrap; overflow-wrap: break-word;">
+                    <div data-contents="true">
+                        <div class="Editable-unstyled" data-block="true" data-editor="5mdts" data-offset-key="av2ij-0-0">
+                            <div data-offset-key="av2ij-0-0" class="public-DraftStyleDefault-block public-DraftStyleDefault-ltr"><span
+                                    data-offset-key="av2ij-0-0"><br data-text="true"></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <button type="button" class="Button AnswerForm-submit Button--primary Button--blue">提交回答</button>
     
     )
   }
